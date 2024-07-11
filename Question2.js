f78582b237c3499cbb3994a06688bd3e8e65334a
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-11 12:37:25 +0500

    Question2 Twosum

---
+++ Question2.js
@@ -0,0 +1,9 @@
+// Two Sum
+
+function twoSum() {
+  let nums = [2, 7, 11, 15];
+  let target = 9;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length - i; j++) {}
+  }
+}
