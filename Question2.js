0365d40b0d52214061d3a5aa350b1682bf6f0cc5
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-11 12:44:28 +0500

    Question Completed

--- Question2.js
+++ Question2.js
@@ -2,8 +2,13 @@
 
 function twoSum() {
   let nums = [2, 7, 11, 15];
-  let target = 9;
+  let target = 17;
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length - i; j++) {}
+    for (let j = i + 1; j < nums.length; j++) {
+      if (target == nums[i] + nums[j]) {
+        return [i, j];
+      }
+    }
   }
 }
+twoSum();
