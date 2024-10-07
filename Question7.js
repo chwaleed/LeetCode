c1cb149008b36b1e2f333d84e247d8a4c8e9d3a8
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-10-08 00:16:11 +0500

    Question 7 have been solved

--- Question7.js
+++ Question7.js
@@ -1,15 +1,23 @@
-const bracket = "([])";
+const s = "()";
 
 const openBracket = "({[";
 const closeBracket = ")}]";
-let stack = [];
+let stack = ["9", "10"];
 
-for (let i = 0; i < bracket.length; i++) {
-  if (openBracket.includes(bracket[i])) {
-    stack.push(bracket[i]);
+for (let i = 0; i < s.length; i++) {
+  if (openBracket.includes(s[i])) {
+    stack.push(s[i]);
     console.log(stack);
-  } else if (closeBracket.includes(bracket[i])) {
-    stack.pop();
+  } else if (closeBracket.includes(s[i])) {
+    const lastOpen = stack.pop(); // Get the last open bracket
+    if (
+      (s[i] === ")" && lastOpen === "(") ||
+      (s[i] === "}" && lastOpen === "{") ||
+      (s[i] === "]" && lastOpen === "[")
+    ) {
+    } else {
+      return false; // Mismatched brackets
+    }
   } else {
     return false;
   }
