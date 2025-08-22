bd42e4beace70d0966b8a6ef848b04a36adec7cd
Waleed <chwaleedmueez@gmail.com>
2025-08-22 10:43:14 +0500

    question 110 solved

---
+++ Question-110.js
@@ -0,0 +1,31 @@
+// 110. Balanced Binary Tree
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+  function dfs(node) {
+    if (!node) return 0;
+
+    let left = dfs(node.left);
+    if (left === -1) return -1;
+
+    let right = dfs(node.right);
+    if (right === -1) return -1;
+
+    if (Math.abs(left - right) > 1) return -1;
+
+    return 1 + Math.max(left, right);
+  }
+
+  return dfs(root) !== -1;
+};
